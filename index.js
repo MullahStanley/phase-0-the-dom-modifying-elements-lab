c25bbc46c88a611f7b9da724d1b35fd5550b5010
MullahStanley <mullah.kimani@student.moringaschool.com>
2023-03-23 13:42:07 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main= document.querySelector("#main");
+main.remove();
+
+const newHeader= document.createElement('h1')
+newHeader.setAttribute('id','victory')
+document.body.append(newHeader)
+newHeader.innerText="MUllah is the champion"
